ecfd50782526f0194f3e2659ea9acfde94de948a
Abdel-Hamid Abdelkerim Mahamat <abdelhamidabdelkerim1@gmail.com>
2021-12-01 23:54:17 +0300

    Header and Footer added to main page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,17 +4,31 @@
   <title>@yield('title')</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  @yield('CSS')
-  @yield('JS')
+  <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+
+    <title>Pixie - Ecommerce HTML5 Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/tooplate-main.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+<!--
+Tooplate 2114 Pixie
+https://www.tooplate.com/view/2114-pixie
+-->
 </head>
 <body>
 
 
 <div class="jumbotron text-center">
   <h1>Header</h1>
+  @include('Home._header')
   @yield('header')
 </div>
   
@@ -40,6 +54,7 @@
 <div class="jumbotron text-center">
   <h1>Footer</h1>
   @yield('footer')
+  @include('Home._footer')
 </div>
 </body>
 </html>
